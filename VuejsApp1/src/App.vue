ace080f475d6abfeeb9e68bfdbbe11fe941b0f75
Kathrine <61585423+Kat1606@users.noreply.github.com>
2021-05-17 09:04:29 +0200

    Add project files.

---
+++ VuejsApp1/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div id="app">
+        <router-view></router-view>
+        <button id="login">
+            <a><router-link to="/Home">Login</router-link></a>
+        </button>
+    </div>
+</template>
+
+
+<script>
+    import Home from './components/Home.vue';
+
+    export default {
+        name: 'app',
+        components: {
+            Home
+        }
+    };
+</script>
+
+<style>
+</style>
+
